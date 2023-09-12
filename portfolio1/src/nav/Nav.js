11c18e21f5811f488b08c6ebec9a481c04d804e4
Jon9851 <joncohen21k@hotmail.co.uk>
2023-09-12 12:20:33 +0000

    icons added unsure about desgin yet subject to change

--- portfolio1/src/nav/Nav.js
+++ portfolio1/src/nav/Nav.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import {Link, useLocation} from "react-router-dom"
+import laptop from "../assets/laptop.png"
+import about from "../assets/about.png"
+import project from "../assets/project.png"
+import envelope from "../assets/envelope.png"
+import "../styles/nav.css"
 
 export default class Nav extends Component {
   render() {
     return (
-      <div>Nav</div>
-    )
+      <div>
+        <img src={laptop} />
+        <img src={about} />
+        <img src={project} />
+        <img src={envelope} />
+      </div>
+    );
   }
 }
