afa2239476b8b1c5dcc1c54718fbe2314b186919
Jon9851 <joncohen21k@hotmail.co.uk>
2023-09-14 15:31:59 +0000

    skills menu added along with proejcts page, still not happy with overall design and looks of the page

---
+++ portfolio1/src/projects/projectsData.js
@@ -0,0 +1,65 @@
+import projectOne from "../assets/project-1.png";
+import projectTwo from "../assets/project-2.png";
+import projectThree from "../assets/project-3.png";
+import projectFour from "../assets/project-4.png";
+
+const projects = {
+  1: {
+    title: "SoMe Platform",
+    image: projectOne,
+    description: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </>
+    ),
+    github: "https://github.com/Jon9851/You-Matter-",
+    demo: "https://netlify.com",
+  },
+  2: {
+    title: "Productivity App",
+    image: projectTwo,
+    description: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </>
+    ),
+    github: "https://github.com/Jon9851/History-Quiz",
+    demo: "https://netlify.com",
+  },
+  3: { 
+    title: "Food Delivery App",
+    image: projectThree,
+    description: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </>
+    ),
+    github: "https://github.com/Jon9851/Game-Review",
+    demo: "https://netlify.com",
+},
+
+4: {
+    title: "Food Delivery App",
+    image: projectFour,
+    description: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </>
+    ),
+    github: "https://github.com/Jon9851/Game-Review",
+    demo: "https://netlify.com",
+  },
+};
+export default projects;
---
+++ portfolio1/src/skills/skillsData.js
@@ -0,0 +1,51 @@
+const skills = {
+    1: [
+      {
+        title: "HTML",
+        level: 6,
+      },
+      {
+        title: "CSS",
+        level: 5,
+      },
+      {
+        title: "Django",
+        level: 2,
+      },
+      {
+        title: "JavaScript",
+        level: 3,
+      },
+      
+      {
+        title: "React JS",
+        level: 1,
+      },
+   
+    ],
+    2: [
+      {
+        title: "PostgreSQL",
+        level: 3,
+      },
+      {
+        title: "Node.js",
+        level: 1,
+      },
+      {
+        title: "MongoDB",
+        level: 2,
+      },
+      {
+        title: "Git",
+        level: 4,
+      },
+      
+      {
+        title: "SQL",
+        level: 3,
+      },
+    ],
+  };
+  
+  export default skills;
